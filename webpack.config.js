3d6d703ea247e2200ea11914e60f389f63b4e8e1
Bas Cornelissen <bjmcornelissen@gmail.com>
2018-01-06 18:08:15 +0100

    dist

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const webpack = require('webpack');
 
 module.exports = {
@@ -46,6 +47,9 @@ module.exports = {
       title: 'Development',
     }),
     new webpack.NamedModulesPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
+    new UglifyJsPlugin({
+      test: /\.js($|\?)/i,
+    }),
   ],
 };
